725a14d3879d87416587a2c96dc25d30ca5b55dd
Brian Sullivan <brian@civicevolution.org>
2013-04-18 20:55:45 -0700

    Cleanup the firebase code and move it into a module
    Move the firebase connection code into a separate module and include it
    in the comment model
    Remove the firebase related code out of the comment module

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,17 +1,12 @@
 class Comment < ActiveRecord::Base
+  include Modules::FirebaseConnect
+  
   attr_accessible :liked, :name, :text
-  
-  #validates :password, :length => { :in => 6..20
-  
+
+  before_create { self.liked ||= false; true }
+
   validate :my_test
-  
-  before_save do |comment|
-    comment.liked ||= false
-    true
-  end
-  
-  attr_accessor :_is_new_record
-  
+
   def my_test
     #errors.add(:text, "My text error message")
     #errors.add(:name, "My name error message")
@@ -20,23 +15,4 @@ class Comment < ActiveRecord::Base
     #false
   end
 
-  def as_json_for_firebase( action = "create" )
-    data = as_json root: false, except: [:created_at, :updated_at]
-    #{ self.class.to_s.to_sym => data, :action => action }
-    { "class" => self.class.to_s, "action" => action, "data" => data, "source" => "RoR-Firebase" }
-  end
-
-
-  before_create { self._is_new_record = true }
-  after_save :send_to_firebase
-  after_destroy :send_to_firebase
-  
-  def send_to_firebase
-    #Firebase.auth = "LHp51r7znXmO09dACFIz4TLPp7zbrdMHPtVkHua2"  
-    action = _is_new_record ? "create" : destroyed? ? "destroy" : "update"
-    Firebase.base_uri = 'https://civicevolution.firebaseio.com/issues/7/updates'  
-    response = Firebase.push '', self.as_json_for_firebase( action )
-  end
-  
-  
 end
---
+++ lib/modules/firebase_connect.rb
@@ -0,0 +1,28 @@
+module Modules
+  module FirebaseConnect
+    extend ActiveSupport::Concern
+
+    attr_accessor :_is_new_record
+
+    included do
+      before_create { self._is_new_record = true }
+      after_save :send_to_firebase
+      after_destroy :send_to_firebase
+    end
+    
+    
+    def send_to_firebase
+      #debugger
+      #Firebase.auth = "LHp51r7znXmO09dACFIz4TLPp7zbrdMHPtVkHua2"  
+      action = _is_new_record ? "create" : destroyed? ? "destroy" : "update"
+      Firebase.base_uri = 'https://civicevolution.firebaseio.com/issues/7/updates'  
+      response = Firebase.push '', self.as_json_for_firebase( action )
+    end
+
+    def as_json_for_firebase( action = "create" )
+      data = as_json root: false, except: [:created_at, :updated_at]
+      #{ self.class.to_s.to_sym => data, :action => action }
+      { "class" => self.class.to_s, "action" => action, "data" => data, "source" => "RoR-Firebase" }
+    end
+  end
+end
