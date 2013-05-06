b18573db48893e9726f5549a90a143003a61d181
Brian Sullivan <brian@civicevolution.org>
2013-05-05 18:29:53 -0700

    Fix initial controller specs so all specs pass

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,10 +3,12 @@
 FactoryGirl.define do
   factory :user do
     name 'Test User'
+    first_name 'Test'
+    last_name 'User'
     email 'example@example.com'
     password 'changeme'
     password_confirmation 'changeme'
     # required if the Devise Confirmable module is used
-    # confirmed_at Time.now
+    confirmed_at Time.now
   end
 end
