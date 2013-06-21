588b1de65ef31fbb1f7a65abe6da3c7c4317ec69
Brian Sullivan <brian@civicevolution.org>
2013-06-21 16:00:45 -0700

    Tell Production to use DEV S3 assets until I can copy them to PROD bucket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,6 +112,6 @@ Ce2::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  RES_BASE='civic'
+  RES_BASE='civic_dev'
 
 end
