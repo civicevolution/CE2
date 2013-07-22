3d0ccba234ea59ef5c09ae50301567a4df013ae6
Brian Sullivan <brian@civicevolution.org>
2013-07-22 13:41:14 -0700

    Ignore execute "create extension hstore" b/c rails user doesn't have privileges to execute this

--- db/migrate/20130503173036_create_conversations.rb
+++ db/migrate/20130503173036_create_conversations.rb
@@ -1,6 +1,6 @@
 class CreateConversations < ActiveRecord::Migration
   def change
-    execute "create extension hstore"
+    #execute "create extension hstore"
     create_table :conversations do |t|
       t.string :code, null: false
       t.integer :user_id, null: false
