3189862ef6800a8880e8c6b7a6ee2b480debbf88
Brian Sullivan <brian@civicevolution.org>
2013-08-06 15:17:00 -0700

    Add user_id to the reply_records so I can easily check if the reply was made notification requester immediate notification is working now for _all and _me find all immediate requests for this comments conversation if immediate_me check the reply_to_targets and mentions for request's user_id Send email if reply_to_target, mention, or immediate_all AND not requester's comment AND not conversation comment edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130806174516) do
+ActiveRecord::Schema.define(version: 20130806204759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -166,6 +166,7 @@ ActiveRecord::Schema.define(version: 20130806174516) do
     t.boolean  "quote"
     t.string   "author"
     t.string   "code"
+    t.integer  "user_id"
   end
 
   create_table "roles", force: true do |t|
--- lib/modules/post_process.rb
+++ lib/modules/post_process.rb
@@ -21,25 +21,6 @@ module Modules
     end
     handle_asynchronously :post_process, :priority => 20, :run_at => Proc.new { 10.seconds.from_now }
 
-    def add_to_activity_feed
-      Rails.logger.debug " I need to implement add_to_activity_feed"
-    end
-
-    def check_immediate_notifications
-
-      return if !@_is_new_record && self.type == 'ConversationComment'
-
-      Rails.logger.debug " I need to implement check_immediate_notifications"
-      # any requests for this conversation with immediate_all
-      requests = self.conversation.notification_requests.includes(:user).where(immediate_all: true)
-      requests.each do |request|
-        Rails.logger.debug "Email post to #{request.user.email}"
-        ConversationMailer.delay.immediate_notification(request.user, self, "mcode", "host")
-      end
-
-    end
-
-
     # When do I create a record
     # comment params has in_reply_to_id and in_reply_to_version keys
     # comment text has embedded quotes
@@ -81,7 +62,7 @@ module Modules
     # maintain the reply record if this was a quote
 
     def record_replies
-      Rails.logger.debug "Post process record replies"
+      #Rails.logger.debug "Post process record replies"
 
       reply_to_records = []
 
@@ -93,7 +74,7 @@ module Modules
         #puts match[0]
         params = match[0].split(/~/)
         reply_to_records.push Reply.new comment_id: self.id, reply_to_id: params[2], version: params[4], quote: true
-        puts "com_id #{params[2]}"
+        #puts "com_id #{params[2]}"
       end
 
       # now remove duplicate records, but retain any record with quote = false
@@ -118,6 +99,7 @@ module Modules
           com = comments.detect{|c| c.id == reply.reply_to_id}
           reply.author = "#{com.author.first_name} #{com.author.last_name}"
           reply.code = com.author.code
+          reply.user_id = com.user_id
         end
       end
 
@@ -142,9 +124,9 @@ module Modules
     #      Add them to the association with <<
 
     def update_replies_collection(comment,reply_to_records )
-      reply_to_records.each do |rec|
-        puts "pending new rec #{rec.inspect}"
-      end
+      #reply_to_records.each do |rec|
+      #  puts "pending new rec #{rec.inspect}"
+      #end
 
       unused_reply_records_to_destroy = []
       comment.reply_to_targets.each do |current_reply_rec|
@@ -153,11 +135,11 @@ module Modules
           reply_to_records.reject!{|r| r.comment_id == current_reply_rec.comment_id && r.reply_to_id == current_reply_rec.reply_to_id && r.quote == false }
         else
           rec = reply_to_records.detect{|r| r.comment_id == current_reply_rec.comment_id && r.reply_to_id == current_reply_rec.reply_to_id }
-          puts "rec: #{rec}"
+          #puts "rec: #{rec}"
           if rec # if it is part of assoc remove from the pending list
             reply_to_records.reject!{|r| r.comment_id == current_reply_rec.comment_id && r.reply_to_id == current_reply_rec.reply_to_id }
           else # if it is part of assoc and not in the pending list, mark for removal from assoc
-            puts "Mark for removal #{current_reply_rec.inspect}"
+            #puts "Mark for removal #{current_reply_rec.inspect}"
             unused_reply_records_to_destroy.push current_reply_rec
             #current_reply_rec.comment_id = nil # mark to delete
           end
@@ -169,14 +151,14 @@ module Modules
       end
 
       reply_to_records.each do |rec|
-        puts "add new rec to reply_to_records #{rec.inspect}"
+        #puts "add new rec to reply_to_records #{rec.inspect}"
         comment.reply_to_targets << rec
       end
     end
 
 
     def record_mentions
-      Rails.logger.debug " I need to implement record_mentions"
+      #Rails.logger.debug " I need to implement record_mentions"
       # parse comment text for \s\@\w+\b
 
       mentioned_names = self.text.scan(/(?<!\w)@\w+/).map{|n| n.sub('@','') }.uniq
@@ -198,6 +180,52 @@ module Modules
         Firebase.push '', self.as_json_for_firebase( action )
       end
     end
+
+    #
+    # Immediate notification has two modes
+    # immediate_all - user wants to be notified of every post (do not send post to its author)
+    # immediate_me - user wants to be notified of every post that is a reply to their post or mentions them (do not send post to its author)
+    #
+
+    def check_immediate_notifications
+      # no immediate notification on edited conversation comments (what about a new mention?)
+      return if !@_is_new_record && self.type == 'ConversationComment'
+
+      #Rails.logger.debug "I need to implement check_immediate_notifications"
+      # any requests for this conversation with immediate_all
+      #requests = self.conversation.notification_requests.includes(:user).where(immediate_all: true)
+      requests = self.conversation.notification_requests.includes(:user).where('immediate_all = true OR immediate_me = true')
+      requests.each do |request|
+        next if request.user_id = self.user_id # don't send notification of a comment to the author of the comment
+        # if immediate_all, just send the notification
+        # if immediate_me, check if the notification mentions or replies to me
+        if request.immediate_me
+          # check self.reply_to_targets for my user_id
+          index = self.reply_to_targets.index{|r| r.user_id == request.user_id }
+          if index.nil?
+            # if no reply check for a mention
+            # check self.mentions for my user_id as mentioned_user_id
+            index = self.mentions.index{|m| m.mentioned_user_id == request.user_id }
+          end
+          # if no reply or mention, go on to the next request
+          if index.nil?
+            next
+          end
+        end
+        # either reply or mention match for immediate_me or immediate_all - so send now
+        #Rails.logger.debug "Email post to #{request.user.email}"
+        ConversationMailer.delay.immediate_notification(request.user, self, "mcode", "host")
+      end
+    end
+
+
+
+    def add_to_activity_feed
+      Rails.logger.debug "I need to implement add_to_activity_feed"
+    end
+
+
+
 
     # override as_json_for_firebase in the model as needed
     def as_json_for_firebase( action = "create" )
