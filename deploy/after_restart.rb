1d4b45ad83d95ef365ced5285d4235a308eacd32
Brian Sullivan <brian@civicevolution.org>
2013-08-07 07:55:21 -0700

    Add the deploy after_restart and before_restart restart delayed_job

---
+++ deploy/after_restart.rb
@@ -0,0 +1,8 @@
+begin
+  #app = release_path.to_s.match(/\/data\/(\w+)/)[1]
+  #if app == 'idea'
+     sudo 'echo "sleep 180 && sudo monit restart delayed_job_ce2_beta" | at now'
+  #end
+rescue Exception => e
+  run "echo 'deploy/after_restart.rb error: #{e.message}"
+end
