b3ff47772ccc8359b7386188740793cdf1c6aa28
Brian Sullivan <brian@civicevolution.org>
2013-09-20 08:57:03 -0700

    A few changes before pushing the merger of ce-live into master back to github Remove migrations to add and remove unused table

--- db/migrate/20130918220302_drop_table_allocation_items.rb
+++
@@ -1,5 +0,0 @@
-class DropTableAllocationItems < ActiveRecord::Migration
-  def change
-    drop_table :allocation_items
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130918220302) do
+ActiveRecord::Schema.define(version: 20130917171536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
