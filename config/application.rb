d2a2f2c0718dc1ce2d8bf6d04dcb3397cd59e40e
Brian Sullivan <brian@civicevolution.org>
2013-10-08 02:37:13 -0700

    disable assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -51,6 +51,11 @@ module Ce2
     config.autoload_paths += %W(#{config.root}/lib)
 
 
+
+    config.assets.enabled = false
+
+
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
