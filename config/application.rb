dcf4e2d0f1b8262584e92e3844bce49e45cb16c7
Brian Sullivan <brian@civicevolution.org>
2013-10-08 02:47:38 -0700

    try to precoompile the menu templates

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,8 @@ module Ce2
     config.assets.enabled = false
 
 
+    config.assets.precompile += %w( live/menu/small-group-deliberation.html live/menu/theme-allocation
+
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
