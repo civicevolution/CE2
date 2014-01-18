515df46b325c0c44b9519073dc01046be9c940da
Brian Sullivan <brian@civicevolution.org>
2014-01-18 00:11:25 -0800

    Update before_migrate to precompile assets

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,16 +1,14 @@
 Chef::Log.info("Running deploy/before_migrate.rb...")
 
-#Chef::Log.info("Symlinking #{release_path}/public/assets to #{new_resource.deploy_to}/shared/assets")
-#
-#link "#{release_path}/public/assets" do
-#  to "#{new_resource.deploy_to}/shared/assets"
-#end
+rails_env = new_resource.environment["RAILS_ENV"]
+Chef::Log.info("Precompiling assets for #{rails_env}...")
 
-#rails_env = new_resource.environment["RAILS_ENV"]
-#Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
-#
-#execute "rake assets:precompile" do
-#  cwd release_path
-#  command "bundle exec rake assets:precompile"
-#  environment "RAILS_ENV" => rails_env
-#end
+current_release = release_path
+
+Chef::Log.info("Precompiling assets into #{release_path}...")
+
+execute "rake assets:precompile" do
+  cwd current_release
+  command "bundle exec rake assets:precompile"
+  environment "RAILS_ENV" => rails_env
+end
