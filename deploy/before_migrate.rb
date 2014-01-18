28af4b50d662c9083b69ee32bbe900656f39a86e
Brian Sullivan <brian@civicevolution.org>
2014-01-17 16:26:37 -0800

    Remove symlink from before migrate file & precompile

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -6,11 +6,11 @@ Chef::Log.info("Running deploy/before_migrate.rb...")
 #  to "#{new_resource.deploy_to}/shared/assets"
 #end
 
-rails_env = new_resource.environment["RAILS_ENV"]
-Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
-
-execute "rake assets:precompile" do
-  cwd release_path
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => rails_env
-end
+#rails_env = new_resource.environment["RAILS_ENV"]
+#Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
+#
+#execute "rake assets:precompile" do
+#  cwd release_path
+#  command "bundle exec rake assets:precompile"
+#  environment "RAILS_ENV" => rails_env
+#end
