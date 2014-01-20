7fdca9f709ee12f8cf3aaf363be0e0b4e5dea000
Brian Sullivan <brian@civicevolution.org>
2014-01-20 00:21:01 -0800

    Add before_symlink.rb to run db:seed - don't do seed if pending migrations - fix

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,17 +1,12 @@
 Chef::Log.info("Running deploy/before_symlink.rb...")
 
-pending_migrations = ActiveRecord::Migrator.open(ActiveRecord::Migrator.migrations_paths).pending_migrations
-if pending_migrations.any?
-  Chef::Log.info("Running deploy/before_symlink.rb - RUN migration before db:seed can run...")
-else
+current_release = release_path
 
-  current_release = release_path
+execute "rake db:seed" do
+  cwd current_release
+  command "bundle exec rake db:seed"
 
-  execute "rake db:seed" do
-    cwd current_release
-    command "bundle exec rake db:seed"
+  not_if do
+    ActiveRecord::Migrator.open(ActiveRecord::Migrator.migrations_paths).pending_migrations.any?
   end
-
 end
-
-
