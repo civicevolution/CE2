e0121bc124e3073583341d6b0280c4a1871d3a7c
Brian Sullivan <brian@civicevolution.org>
2014-01-19 23:44:10 -0800

    Add before_symlink.rb to run db:seed - ignore exit code

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,8 +1,10 @@
 Chef::Log.info("Running deploy/before_symlink.rb...")
 
-current_release = release_path
+#current_release = release_path
+#
+#execute "rake db:seed" do
+#  cwd current_release
+#  command "bundle exec rake db:seed"
+#end
 
-execute "rake db:seed" do
-  cwd current_release
-  command "bundle exec rake db:seed"
-end
+run_and_ignore_exitcode_and_print_command "cd #{release_path} && /usr/local/bin/bundle exec rake db:seed"
