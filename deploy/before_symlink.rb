e884a04611a51b16d5f1d3dc894b2f9a7194bf41
Brian Sullivan <brian@civicevolution.org>
2014-01-19 23:53:34 -0800

    Add before_symlink.rb to run db:seed - add begin/rescue

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,10 +1,11 @@
 Chef::Log.info("Running deploy/before_symlink.rb...")
 
-#current_release = release_path
-#
-#execute "rake db:seed" do
-#  cwd current_release
-#  command "bundle exec rake db:seed"
-#end
+current_release = release_path
 
-run_and_ignore_exitcode_and_print_command "cd #{release_path} && /usr/local/bin/bundle exec rake db:seed"
+begin
+  execute "rake db:seed" do
+    cwd current_release
+    command "bundle exec rake db:seed"
+  end
+rescue
+end
