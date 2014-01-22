0476d2ae23ba4df04806f0c652fe420d28b57f1c
Brian Sullivan <brian@civicevolution.org>
2014-01-22 08:21:15 -0800

    Disabled before_migrate ans before_symlink

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,13 +1,13 @@
-Chef::Log.info("Running deploy/before_migrate.rb...")
+Chef::Log.info("^^^^^^ v1-22-8:20 Running deploy/before_migrate.rb...")
 
-rails_env = new_resource.environment["RAILS_ENV"]
-Chef::Log.info("Precompiling assets for #{rails_env}...")
-
-Chef::Log.info("v1-21-23:54 Precompiling assets into #{current_release}...")
-
-execute "rake assets:precompile" do
-  cwd current_release
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => rails_env
-  ignore_failure true
-end
+#rails_env = new_resource.environment["RAILS_ENV"]
+#Chef::Log.info("Precompiling assets for #{rails_env}...")
+#
+#Chef::Log.info("Precompiling assets into #{current_release}...")
+#
+#execute "rake assets:precompile" do
+#  cwd current_release
+#  command "bundle exec rake assets:precompile"
+#  environment "RAILS_ENV" => rails_env
+#  ignore_failure true
+#end
--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,7 +1,7 @@
-Chef::Log.info("Running deploy/before_symlink.rb...")
+Chef::Log.info("^^^^^^ v1-22-8:20 Running deploy/before_symlink.rb...")
 
-execute "rake db:seed" do
-  cwd release_path
-  command "bundle exec rake db:seed"
-  ignore_failure true
-end
+#execute "rake db:seed" do
+#  cwd release_path
+#  command "bundle exec rake db:seed"
+#  ignore_failure true
+#end
