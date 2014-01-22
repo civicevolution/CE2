e14fbadad8938d6e4fe20f5696fa15433f5f13cc
Brian Sullivan <brian@civicevolution.org>
2014-01-22 10:39:55 -0800

    restore before_migrate 'current_release'

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -3,6 +3,8 @@ Chef::Log.info("^^^^^^ v1-22-10:32 Running deploy/before_migrate.rb...")
 rails_env = new_resource.environment["RAILS_ENV"]
 Chef::Log.info("Precompiling assets for #{rails_env}...")
 
+current_release = release_path
+
 Chef::Log.info("Precompiling assets into #{current_release}...")
 
 execute "rake assets:precompile" do
