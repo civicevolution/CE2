aa19a1e338b83ff2e85b3527f6b3ee5af1f5f270
Brian Sullivan <brian@civicevolution.org>
2014-01-21 23:56:13 -0800

    Add timestamp to before_migrate.rb

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -3,9 +3,7 @@ Chef::Log.info("Running deploy/before_migrate.rb...")
 rails_env = new_resource.environment["RAILS_ENV"]
 Chef::Log.info("Precompiling assets for #{rails_env}...")
 
-current_release = release_path
-
-Chef::Log.info("Precompiling assets into #{release_path}...")
+Chef::Log.info("v1-21-23:54 Precompiling assets into #{current_release}...")
 
 execute "rake assets:precompile" do
   cwd current_release
