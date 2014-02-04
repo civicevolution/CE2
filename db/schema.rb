8709b25a825bdbf207e9fb7757a37b8cb5c61e06
Brian Sullivan <brian@civicevolution.org>
2014-02-04 12:46:00 -0800

    Rolled back the test commit from schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140204180733) do
+ActiveRecord::Schema.define(version: 20140204175931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -523,10 +523,6 @@ ActiveRecord::Schema.define(version: 20140204180733) do
     t.integer  "admin_user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "test_tables", force: true do |t|
-    t.integer "id_957"
   end
 
   create_table "theme_points", force: true do |t|
