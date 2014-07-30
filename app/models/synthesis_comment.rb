939c699bae0b693ab15aecdde74d55173c007307
Brian Sullivan <brian@civicevolution.org>
2014-07-30 13:15:19 -0700

    validate synthesis comment that it only has one tag

--- app/models/synthesis_comment.rb
+++ app/models/synthesis_comment.rb
@@ -21,20 +21,21 @@ class SynthesisComment < Comment
     self.order_id = SynthesisComment.where(conversation_id: self.conversation_id).maximum(:order_id).try{ |max| max + 1 } || 1
   end
 
-  before_save :check_tag_ids
+  validate :check_tag_ids
   after_save :update_tag_ids
 
   def check_tag_ids
-    Rails.logger.debug "check_tag_ids make sure 0 or 1 only, tag_ids: #{tag_ids}"
+    #Rails.logger.debug "check_tag_ids make sure 0 or 1 only, tag_ids: #{tag_ids}"
     if tag_ids && tag_ids.size > 1
       errors.add(:tag_ids, "Comment can only have one tag")
+      return false
     end
   end
 
   def update_tag_ids
-    Rails.logger.debug "check if tag_ids defined"
+    #Rails.logger.debug "check if tag_ids defined"
     if !tag_ids.nil?
-      Rails.logger.debug "process the tag_ids to make sure it is up-to-date"
+      #Rails.logger.debug "process the tag_ids to make sure it is up-to-date"
       CommentTagAssignment.updateCommentTagAssignments(self)
     end
   end
