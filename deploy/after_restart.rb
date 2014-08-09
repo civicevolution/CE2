dd0e14c96874c9c158749682ab9d8c53c69f46fb
Brian Sullivan <brian@civicevolution.org>
2014-08-08 23:04:36 -0700

    Restore handle_asynchronously :post_process change after_restart.rb to  sudo 'echo "sleep 60 && monit restart delayed_job_ce2_ver_1" | at now'

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,7 +1,8 @@
 begin
   #app = release_path.to_s.match(/\/data\/(\w+)/)[1]
   #if app == 'idea'
-     sudo 'echo "sleep 180 && sudo monit restart delayed_job_ce2_ver_1" | at now'
+    Rails.logger.info "Restart: monit restart delayed_job_ce2_ver_1 "
+     sudo 'echo "sleep 60 && monit restart delayed_job_ce2_ver_1" | at now'
   #end
 rescue Exception => e
   run "echo 'deploy/after_restart.rb error: #{e.message}"
