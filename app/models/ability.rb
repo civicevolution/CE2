be86e0de46a4ab5ec8306b9b04960e7803f725b7
Brian Sullivan <brian@civicevolution.org>
2014-08-14 17:49:18 -0700

    Prevent the remote group member from voting

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -15,7 +15,7 @@ class Ability
   @@conversation_actions_by_role[:curator] = %i( edit_synthesis edit_summary update_comment_order edit_cta group_message approve_posts approve_participants moderate_posts ).concat @@conversation_actions_by_role[:trusted_participant]
   @@conversation_actions_by_role[:conversation_admin] = %i( edit_title privacy tags schedule publish update_role ).concat @@conversation_actions_by_role[:curator]
 
-  @@conversation_actions_by_role[:remote] = %i( conversation_rating edit_my_table_comment edit_synthesis tag_comment view_table_comments edit_table_comment post_prescreen history vote )
+  @@conversation_actions_by_role[:remote] = %i( edit_my_table_comment edit_synthesis tag_comment view_table_comments edit_table_comment post_prescreen history vote )
   @@conversation_actions_by_role[:scribe] = %i( conversation_rating edit_my_table_comment edit_synthesis tag_comment view_table_comments edit_table_comment post_prescreen history vote )
   @@conversation_actions_by_role[:themer] = %i( delete_any_tag edit_all_table_comments edit_synthesis edit_table_comment edit_theme_comment assign_comment_theme post_prescreen history update_comment_order destroy_theme_comment hide).concat @@conversation_actions_by_role[:scribe]
   @@conversation_actions_by_role[:coordinator] = %i( coordinate edit_synthesis publish_themes).concat @@conversation_actions_by_role[:themer]
